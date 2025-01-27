0e2a9d5b20c9479a6bfc86840663c4940fe938a4
solanadeveloper10 <solanadeveloper10@gmail.com>
2025-01-27 17:35:32 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useRef, useEffect, useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [bgLoaded, setBgLoaded] = useState(false)
+
+  const audioRef = useRef(null)
+  const videoRef = useRef(null)
+
+  useEffect(() => {
+    videoRef.current.playbackRate = 2;
+  }, [])
+
+  useEffect(() => {
+    if (bgLoaded) {
+      audioRef.current.play();
+    }
+  }, [bgLoaded])
+
+
+  return (
+    <>
+      <audio id="myAudio" loop="true" ref={audioRef}>
+        <source src="/letsgoriceniggas.m4a" type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+      <div className='static-bg' />
+      <video autoPlay muted id="bgVideo" ref={videoRef} onEnded={() => setBgLoaded(true)}>
+        <source src="/bg_video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className='wrapper'>
+        <header className={'header ' + (bgLoaded ? 'loaded' : '')}>
+          <a href="#" className='social-link'>
+            X
+          </a>
+          <a href="#" className='social-link'>
+            Telegram
+          </a>
+          <a href="#" className='social-link'>
+            Dextools
+          </a>
+          <a href="#" className='social-link'>
+            Dexscreener
+          </a>
+          <a href="#" className='social-link'>
+            Raydium
+          </a>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default App
